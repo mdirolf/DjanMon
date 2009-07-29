5d8027b21b640439e0bf645fc8035f14576f35ec
Mike Dirolf <mike@10gen.com>
2009-07-29 15:09:19 -0400

    add status app

---
+++ status/models.py
@@ -0,0 +1,5 @@
+# from django.db import models
+
+# We don't need models with MongoDB. If you really *want* models check out
+# something like MongoKit (http://bitbucket.org/namlook/mongokit/) to add
+# an ORM-ish layer on top of PyMongo.
--- urls.py
+++ urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     # Example:
     # (r'^DjanMon/', include('DjanMon.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
